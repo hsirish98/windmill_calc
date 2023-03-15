1bea3bdb80c73ee823afa487e8db557f436fb585
hsirish98 <hannahsirish@gmail.com>
2023-03-15 10:59:15 -0700

    made data and data help page!

---
+++ R/turbines_wind.R
@@ -0,0 +1,12 @@
+#' Speeds and wind turbine blade lengths
+#'
+#' Data from from Colwell, Snyder, and Irish, LLC
+#'
+#' @format A data frame with 100 rows and 2 variables
+#' \itemize{
+#' \item speeds (m/s)
+#' \item blade_lengths (meters)
+#' }
+#'@source \url{colwell_snyder_irish.com}
+#'
+"turbines_wind"
