c0438c7f2d969dc1bbd04a222e49a47b708e8c20
hsirish98 <hannahsirish@gmail.com>
2023-03-15 11:26:15 -0700

    finished assignment

--- R/turbines_wind.R
+++ R/turbines_wind.R
@@ -2,7 +2,7 @@
 #'
 #' Data from from Colwell, Snyder, and Irish, LLC
 #'
-#' @format A data frame with 100 rows and 2 variables
+#' @format A data frame with 100 rows and 2 variables. Variables were collected from surveying averages of U.S. 10 biggest wind farms.
 #' \itemize{
 #' \item speeds (m/s)
 #' \item blade_lengths (meters)
